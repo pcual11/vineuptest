089bf169c650a48b45eb092e23d2a6cd2a9ea06e
Calogero Pumpignano <39948866+pcual11@users.noreply.github.com>
2018-06-07 08:38:58 -0500

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,5 +1,5 @@
 REM Install Internet Information Server (IIS).
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command git -C "C:\vineuptest\" pull --all
+call git -C "C:\vineuptest\" pull --all
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
 
